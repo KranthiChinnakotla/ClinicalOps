2b90ac60b3d56d8a66379aaf7a3d3aff44725678
Kranthi K Chinnakotla <kranthi2@hotmail.com>
2017-07-05 23:20:53 -0400

    new file:   TrialDocs.war 	modified:   TrialDocs/WebContent/WEB-INF/ctrials-servlet.xml 	modified:   TrialDocs/WebContent/WEB-INF/web.xml 	modified:   TrialDocs/src/org/trialdocs/model/UsersDao.java 	modified:   TrialDocs/target/m2e-wtp/web-resources/META-INF/MANIFEST.MF 	modified:   TrialDocs/target/m2e-wtp/web-resources/META-INF/maven/org.trialdocs/TrialDocs/pom.properties

--- TrialDocs/src/org/trialdocs/model/UsersDao.java
+++ TrialDocs/src/org/trialdocs/model/UsersDao.java
@@ -15,7 +15,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-@Component("usersDao")
+@Component("userdao")
 public class UsersDao {
 
 	private NamedParameterJdbcTemplate jdbc;
